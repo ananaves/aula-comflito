3fc069cebc63d97a58c4903822133aea21289d34
Ana Júlia <anajuliaflor2008@gmail.com>
2025-03-07 16:12:53 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflito.ts
+++ conflito.ts
@@ -2,3 +2,6 @@ console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
 console.log(`alunoB inserindo na linha 2`);
 
 let calculo: number = (75 + 75) /3; 
+
+
+console.log(`Inserindo linha 7 pelo alunoB`);
